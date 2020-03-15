79405a9a0d4d36143b5e7b608d5d03b9d15052eb
Kirsten S <kirsten@Kirstens-MacBook-Air.local>
2020-03-15 18:00:51 -0400

    passed removeApple challenge

--- index.js
+++ index.js
@@ -203,8 +203,10 @@ function isItAnApple(strings) {
  * 
  * [2] Invoking `removeApple` with `['a', 'b', 'c' ]` will return `[ 'a', 'b', 'c' ]`.
 */
-function removeApple(/* code here */) {
-  /* code here */
+function removeApple(strings) {
+  return strings.filter((arrItem) => {
+    return (arrItem !== 'apple');
+  });
 }
 
 /**
